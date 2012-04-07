d12f9ad00574c4b3d190f8d6961754cb9756c22f
Jeremy B. Merrill <jeremy@jeremybmerrill.com>
2012-04-06 21:39:35 -0700

    first commit of a basic -- rails new claremonster

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Claremonster::Application.config.secret_token = 'fe9423de782a9f95aaf625fe7e7c9bceefb1f508abf21bb67d25e810e0213f32da46f587bd280d9d86876e1b79e412a2703b6fdf3f86662cf9d5f3c6eb54fdb9'
